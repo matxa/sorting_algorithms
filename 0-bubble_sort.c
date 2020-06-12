3574bea4a5bd50306f6c1ab43d749ea35f6ee364
matxa <matxa21@gmail.com>
2020-06-12 18:47:08 -0400

    bs

---
+++ 0-bubble_sort.c
@@ -0,0 +1,28 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorting algorithm
+ * @array: integer array
+ * @size: size of the array
+ * Return: VOID
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t a;
+	size_t b;
+	int temp;
+
+	for (a = 0; a < size; a++)
+	{
+		for (b = 0; b < size - a - 1; b++)
+		{
+			if (array[b] > array[b + 1])
+			{
+				temp = array[b];
+				array[b] = array[b + 1];
+				array[b + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+}
