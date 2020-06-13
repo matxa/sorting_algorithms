cce8af7a107966b466b00cd66c9c380a6dca0572
matxa <matxa21@gmail.com>
2020-06-13 10:44:48 -0400

    sorting  list

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,5 +8,19 @@
 
 void insertion_sort_list(listint_t **list)
 {
-    print_list(*list);
+    listint_t current_int = list->n;
+    listint_t temp_next_int = list->next->n;
+    listint_t temp_int;
+
+
+    while (list != NULL)
+    {
+        if (current_int < temp_next_int)
+        {
+            temp_int = current_int;
+            current_int = temp_next_int;
+            temp_next_int = temp_int;
+            print_list(*list);
+        }
+    }
 }
