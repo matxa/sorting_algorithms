7b91878941c29de8ac79e473bd4efb13a5c952e9
matxa <matxa21@gmail.com>
2020-06-13 10:30:26 -0400

    check for init list

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+
+/**
+* insertion_sort_list - doubly linked list inserion sort
+* @list: list to sort
+* Return: VOID
+*/
+
+void insertion_sort_list(listint_t **list)
+{
+    print_list(list);
+}
