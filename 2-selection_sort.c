89613e64f61d22e2383a4b46d9b6827e4f03414c
matxa <matxa21@gmail.com>
2020-06-14 15:22:31 -0400

    testing v.1

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -24,9 +24,12 @@ void selection_sort(int *array, size_t size)
 				if (array[b] < array[min])
 					min = b;
 			}
-			temp = array[a];
-			array[a] = array[min];
-			array[min] = temp;
+			if (min != a)
+			{
+				temp = array[a];
+				array[a] = array[min];
+				array[min] = temp;
+			}
 		}
 	}
 }
