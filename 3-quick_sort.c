cb751758627a543eb9b10f3854618e3c758428e1
matxa <matxa21@gmail.com>
2020-06-16 11:49:51 -0400

    t

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -27,7 +27,6 @@ void quick_sort_helper(int *array, int low, int high, size_t size)
         quick_sort_helper(array, low, Lomuto_partition - 1, size);
         quick_sort_helper(array, Lomuto_partition + 1, high, size);
     }
-    print_array(array, size);
 }
 
 /**
