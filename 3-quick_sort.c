cd31598a8e15f65198e7ee5681a1b4feeda736f5
matxa <matxa21@gmail.com>
2020-06-15 23:34:30 -0400

    t

---
+++ 3-quick_sort.c
@@ -0,0 +1,67 @@
+#include "sort.h"
+
+/**
+* quick_sort - quick sort
+* @array: array to be sorted
+* @size: size of the array
+* Return: VOID
+*/
+
+void quick_sort(int *array, size_t size)
+{
+    size_t low = array[0];
+    size_t high = array[size];
+    int Lomuto_partition;
+    int array_2[high - low + 1];
+    int top = -1;
+
+    array_2[++top] = low;
+    array_2[++top] = high;
+
+    while (top >= 0) {
+        high = array_2[top--];
+        low = array_2[top--];
+
+        Lomuto_partition = partition(arr, low, high);
+
+        if (Lomuto_partition - 1 > low) { 
+            array_2[++top] = low;
+            array_2[++top] = Lomuto_partition - 1;
+        }
+        if (Lomuto_partition + 1 < high) {
+            array_2[++top] = Lomuto_partition + 1;
+            array_2[++top] = high;
+        }
+    }
+}
+
+/**
+* partition - partitioning function
+* @array: array
+* @low: min
+* @high: max
+* Return: a + 1
+*/
+
+size_t partition(int *array, size_t low, size_t high)
+{
+    size_t pivot = array[high];
+    size_t a = (low - 1);
+    size_t b;
+    size_t temp;
+
+    for (b = low; b <= high - 1; b++)
+    {
+        if (array[b] <= pivot)
+        {
+            a++;
+            temp = array[a];
+            array[a] = array[b];
+            array[b] = temp;
+        }
+    }
+    temp = array[a + 1];
+    array[a + 1] = array[b];
+    array[b] = temp;
+    return (a + 1);
+}
