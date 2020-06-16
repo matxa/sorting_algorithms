3befb015e3d93aa73ce70a5411ba749d78713e94
matxa <matxa21@gmail.com>
2020-06-16 11:34:44 -0400

    t

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -26,8 +26,8 @@ void quick_sort_helper(int *array, int low, int high, size_t size)
 
         print_array(array, size);
         quick_sort_helper(array, low, Lomuto_partition - 1, size);
-        quick_sort_helper(array, Lomuto_partition + 1, high, size);
         print_array(array, size);
+        quick_sort_helper(array, Lomuto_partition + 1, high, size);
     }
 }
 
