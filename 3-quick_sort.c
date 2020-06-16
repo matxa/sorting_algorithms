a838291f2e5ab367dcdb6526374e214d5abf7a6f
matxa <matxa21@gmail.com>
2020-06-16 11:57:22 -0400

    te

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,66 +1,80 @@
 #include "sort.h"
 
 /**
-* quick_sort - quick sort
-* @array: array to be sorted
-* @size: size of the array
-* Return: VOID
-*/
+ * quick_sort - quick sort
+ * @array: array to be sorted
+ * @size: size of the array
+ * Return: VOID
+ */
 
 void quick_sort(int *array, size_t size)
 {
-    int low = 0;
-    int high = size - 1;
+	int low = 0;
+	int high = size - 1;
 
-    quick_sort_helper(array, low, high, size);
-}
-
-void quick_sort_helper(int *array, int low, int high, size_t size)
-{
-    int Lomuto_partition;
-
-    if (low < high)
-    {
-        Lomuto_partition = partition(array, low, high);
-
-        print_array(array, size);
-        quick_sort_helper(array, low, Lomuto_partition - 1, size);
-        quick_sort_helper(array, Lomuto_partition + 1, high, size);
-    }
-    else
-        print_array(array, size);
+	quick_sort_helper(array, low, high, size);
 }
 
 /**
-* partition - partitioning function
-* @array: array
-* @low: min
-* @high: max
-* Return: a + 1
-*/
+ * quick_sort_helper - helper function for quick_sort
+ * @array: array of ints
+ * @low: min
+ * @high: max
+ * @size: size of the array
+ * Return: VOID
+ */
+
+void quick_sort_helper(int *array, int low, int high, size_t size)
+{
+	int Lomuto_partition;
+
+	if (low < high)
+	{
+		Lomuto_partition = partition(array, low, high);
+
+		print_array(array, size);
+		quick_sort_helper(array, low, Lomuto_partition - 1, size);
+		quick_sort_helper(array, Lomuto_partition + 1, high, size);
+	}
+}
+
+/**
+ * partition - partitioning function
+ * @array: array
+ * @low: min
+ * @high: max
+ * Return: a + 1
+ */
 
 int partition(int *array, int low, int high)
 {
-    int pivot = array[high];
-    int a = low - 1;
-    int b;
+	int pivot = array[high];
+	int a = low - 1;
+	int b;
 
-    for (b = low; b <= high - 1; b++)
-    {
-        if (array[b] < pivot)
-        {
-            a++;
-            swap(&array[a], &array[b]);
-        }
-    }
-    swap(&array[a + 1], &array[high]);
-    return (a + 1);
+	for (b = low; b <= high - 1; b++)
+	{
+		if (array[b] < pivot)
+		{
+			a++;
+			swap(&array[a], &array[b]);
+		}
+	}
+	swap(&array[a + 1], &array[high]);
+	return (a + 1);
 }
+
+/**
+ * swap - swap 2 numbers
+ * @a: number to be replaced
+ * @b: number to replace with
+ * Return: VOID
+ */
 
 void swap(int *a, int *b)
 {
-    int temp = *a;
+	int temp = *a;
 
-    *a = *b;
-    *b = temp;
+	*a = *b;
+	*b = temp;
 }
