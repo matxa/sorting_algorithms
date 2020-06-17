e15603ce72ab8e067c565103c0baa4994bf0ddfb
matxa <matxa21@gmail.com>
2020-06-17 10:30:24 -0400

    3

--- sort.h
+++ sort.h
@@ -23,7 +23,7 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 int partition(int *array, int low, int high, size_t size);
 void quick_sort_helper(int *array, int low, int high, size_t size);
-void swap(int* a, int* b);
+void swap(int *a, int *b);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 #endif
