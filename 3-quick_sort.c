b822ff4e52724383571974d074769b06ad8a861b
matxa <matxa21@gmail.com>
2020-06-17 09:55:42 -0400

    3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -30,9 +30,8 @@ void quick_sort_helper(int *array, int low, int high, size_t size)
 
 	if (low < high)
 	{
-		Lomuto_partition = partition(array, low, high);
+		Lomuto_partition = partition(array, low, high, size);
 
-		print_array(array, size);
 		quick_sort_helper(array, low, Lomuto_partition - 1, size);
 		quick_sort_helper(array, Lomuto_partition + 1, high, size);
 	}
@@ -43,10 +42,11 @@ void quick_sort_helper(int *array, int low, int high, size_t size)
  * @array: array
  * @low: min
  * @high: max
+ * @size: size of the array
  * Return: a + 1
  */
 
-int partition(int *array, int low, int high)
+int partition(int *array, int low, int high, size_t size)
 {
 	int pivot = array[high];
 	int a = low - 1;
@@ -61,6 +61,7 @@ int partition(int *array, int low, int high)
 		}
 	}
 	swap(&array[a + 1], &array[high]);
+	print_array(array, size);
 	return (a + 1);
 }
 
