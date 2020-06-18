029f22ec322e65dcdeb100e3a4bb76171038d81c
matxa <matxa21@gmail.com>
2020-06-17 20:48:52 -0400

    tes

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -14,24 +14,32 @@ void insertion_sort_list(listint_t **list)
 
 	while (node)
 	{
-        left = node->prev;
-        right = node;
-        while (right->prev && right->n < left->n)
-        {
-            node = swap_two_nodes(left, right, list);
-            print_list(*list);
-            if (right->prev == NULL)
-                break;
-            left = right->prev;
-        }
+		left = node->prev;
+		right = node;
+		while (right->prev && right->n < left->n)
+		{
+			node = swap_two_nodes(left, right, list);
+			print_list(*list);
+			if (right->prev == NULL)
+				break;
+			left = right->prev;
+		}
 		node = node->next;
-        left = left->next;
+		left = left->next;
 	}
 }
 
+/**
+ * swap_two_nodes - swap 2 nodes in a dubly linked list
+ * @n_1: node to replace
+ * @n_2: node replacing with
+ * @list: list
+ * Return: n_1
+ */
+
 listint_t *swap_two_nodes(listint_t *n_1, listint_t *n_2, listint_t **list)
 {
-    if (n_1->prev)
+	if (n_1->prev)
 		(n_1->prev)->next = n_2;
 	else
 		*list = n_2, n_2->prev = NULL;
@@ -42,33 +50,4 @@ listint_t *swap_two_nodes(listint_t *n_1, listint_t *n_2, listint_t **list)
 	n_1->next = n_2->next;
 	n_2->next = n_1;
 	return (n_1);
-}
-void insertion_sort_help(listint_t **list_h, listint_t *node_to_insert)
-{
-	listint_t *temp;
-	if (*list_h == NULL)
-	{
-		*list_h = node_to_insert;
-		print_list(*list_h);
-	}
-	else if ((*list_h)->n >= node_to_insert->n)
-	{
-		node_to_insert->next = *list_h;
-		node_to_insert->next->prev = node_to_insert;
-		*list_h = node_to_insert;
-		print_list(*list_h);
-	}
-	else
-	{
-		temp = *list_h;
-		while (temp->next != NULL &&
-				temp->next->n < node_to_insert->n)
-			temp = temp->next;
-		node_to_insert->next = temp->next;
-		if (temp->next != NULL)
-			node_to_insert->next->prev = node_to_insert;
-		temp->next = node_to_insert;
-		node_to_insert->prev = temp;
-		print_list(*list_h);
-	}
 }
